724c5084d753394aac6453bd62f9112d21114fd4
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-07-11 04:37:49 +0000

    Se empiezan a probar funcionalidades como recibir parametros de una variable en la url

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Consola
+
 # Create your views here.
 
 from django.http import HttpResponse #para poder hacer una respuesta http
@@ -24,9 +26,17 @@ def tu(request):
 
 
 
-def hola(request, username):
-    print(username)
-    return HttpResponse("<h1>THEGAME {}</h1>".format(username)) #Lo que estamos haciendo aqui es concatenar el parametro que se espera recibir en el HttpResponse 
+#def hola(request, username):
+ #   print(username)
+ #   return HttpResponse("<h1>THEGAME {}</h1>".format(username)) #Lo que estamos haciendo aqui es concatenar el parametro que se espera recibir en el HttpResponse 
+
+
+def my_view(request, *args, **kwargs):
+    print(args)
+    print(kwargs)
+    c = Consola.objects.get(id=kwargs['id'])
+    return HttpResponse("La consola es {}".format(c.consola))
+
 
     
 
