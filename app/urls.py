7857c7f3ddc8ccdb120a9b4b6478b83bd7b0548d
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-07-12 00:42:27 +0000

    Primeras fase del login de usuarios

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,22 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('signup/',views.signup, name='signup'),
     path('categorias/', views.categorias, name='categorias'), 
     path('soporte/', views.soporte, name='soporte'), 
     path('help/', views.ayuda, name='ayuda'), 
-    path('me/', views.tu, name='tu'),
-    path('about/',views.about),
-    path('hola/<int:id>', views.my_view)
-]
+#    path('me/', views.tu, name='tu'),
+
+
+
+
+
+
+    #    path('about/',views.About.as_view()),
+#    path('hola/<int:id>', views.my_view)
+
+
+    ]
 
 
 
